102a2b8d1263cd9faaed87ac9728e68426a3cd47
Aditya Sharma <saditya4063@gmail.com>
2026-06-15 20:44:32 +0530

    Time: 3 ms (61.7%), Space: 43.8 MB (77.66%) - LeetHub

---
+++ 3218-minimum-cost-for-cutting-cake-i/3218-minimum-cost-for-cutting-cake-i.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+        Arrays.sort(horizontalCut);
+        Arrays.sort(verticalCut);
+        int h = horizontalCut.length - 1 , v = verticalCut.length - 1;
+        int hp  = 1 , vp = 1;
+        int cost = 0;
+        while(h >= 0 && v >= 0){
+            if(horizontalCut[h] >= verticalCut[v]){
+                cost += (horizontalCut[h] * vp);
+                hp++;
+                h--;
+            }else{
+                cost += (verticalCut[v] * hp);
+                vp++;
+                v--;
+            }
+        }
+        while(h >= 0){
+            cost += (horizontalCut[h] * vp);
+            hp++;
+            h--;
+        }
+        while(v >= 0){
+            cost += (verticalCut[v] * hp);
+            vp++;
+            v--;
+        }
+        return cost;
+    }
+}
