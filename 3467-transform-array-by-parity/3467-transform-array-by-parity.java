198d7fbd40ea0cc944ae4ed7a2f35cbb856c0c9e
Aditya Sharma <saditya4063@gmail.com>
2026-06-16 23:58:48 +0530

    Time: 1 ms (100%), Space: 46.6 MB (81.64%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,25 +1,20 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int count_zero = 0;
-        int count_one = 0;
-        int Cnum[] = new int[nums.length];
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]%2 == 0){
-                Cnum[i] = 0;
-                count_zero++;
+        int freq[] = new int[2];
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0){
+                freq[0]++;
             }else{
-                Cnum[i] = 1;
-                count_one++;
+                freq[1]++;
             }
         }
-        for(int i=0; i<Cnum.length; i++){
-            if(count_zero>0){
-                Cnum[i] = 0;
-                count_zero--;
-            }else{
-                Cnum[i] = 1;
+        int j = 0;
+        for(int i = 0; i < freq.length;i++){
+            while(freq[i] > 0){
+                nums[j++] = i;
+                freq[i]--;
             }
         }
-        return Cnum;
+        return nums;
     }
 }
