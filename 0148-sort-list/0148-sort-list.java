34e5a09f061a435f0a9fcf699a9ef8f8020e00d2
Aditya Sharma <saditya4063@gmail.com>
2026-06-16 23:18:58 +0530

    Time: 12 ms (28.29%), Space: 58.6 MB (99.43%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
+    public ListNode mergeSort(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        //Find Middle
+        ListNode mid = getMid(head);
+        //Left and right Merge Sort
+        ListNode rightHead = mid.next;
+        mid.next = null;
+        ListNode newLeft = mergeSort(head);
+        ListNode newRight = mergeSort(rightHead);
+        //merge
+        return merge(newLeft ,newRight);
+    }
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    private ListNode merge(ListNode head1 , ListNode head2){
+        ListNode mergedLL = new ListNode(-1);
+        ListNode temp = mergedLL;
+        while(head1 != null && head2 != null) {
+            if(head1.val <= head2.val){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+}
