59dbdd63fb731bb21e16d8ca1b02bc0c9d864527
Aditya Sharma <saditya4063@gmail.com>
2026-06-17 14:10:09 +0530

    Time: 31 ms (38.46%), Space: 47.7 MB (88.46%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public char processStr(String s, long k) {
+        long  len = 0;
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+                if(len > 0){
+                    len--;
+                }
+            }else if(ch == '#'){
+                len *= 2;
+            }else if(ch != '%'){
+                len++;
+            }
+        }
+       if (k >= len) return '.';
+       for(int i = s.length() - 1; i >= 0; i--){
+            char ch = s.charAt(i);
+            if(ch == '*'){
+                len++;
+            }else if(ch == '#'){
+                len /= 2;
+                if(k >= len){
+                    k -= len;
+                }
+                
+            }else if(ch == '%'){
+                k = len - k - 1;
+            }else{
+                if(k + 1 == len){
+                    return ch;
+                }
+                len--;
+            }
+       }
+        return '.';
+    }
+}
