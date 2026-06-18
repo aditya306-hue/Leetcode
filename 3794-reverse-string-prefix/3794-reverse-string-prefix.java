b5b4fb3916cd3f78e68d906f546bbc50526ec802
Aditya Sharma <saditya4063@gmail.com>
2026-06-18 20:25:50 +0530

    Time: 2 ms (49.91%), Space: 44.7 MB (37.72%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String reversePrefix(String s, int k) {
+        StringBuilder sb = new  StringBuilder(s);
+        int start = 0;
+        int end = k - 1;
+        while(start < end){
+            char temp = sb.charAt(start);
+            sb.setCharAt(start , sb.charAt(end));
+            sb.setCharAt(end , temp);
+            start++;
+            end--;
+        }
+        return sb.toString();
+    }
+}
