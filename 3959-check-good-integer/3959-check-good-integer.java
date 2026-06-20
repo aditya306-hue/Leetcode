701d55b30b7d03f2ceb5335acdac892de99b9e63
Aditya Sharma <saditya4063@gmail.com>
2026-06-20 12:42:30 +0530

    Time: 0 ms (100%), Space: 42.6 MB (48.24%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int  digitSum = 0;
+        int sqSum = 0;
+        while(n != 0){
+            int ld = n % 10;
+            digitSum += ld;
+            sqSum += ld * ld;
+            if(sqSum - digitSum >= 50){
+                return true;
+            }
+            n /= 10;
+        }
+        return (sqSum - digitSum) >= 50;
+    }
+}
