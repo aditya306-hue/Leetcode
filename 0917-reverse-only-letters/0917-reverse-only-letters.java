7020f9e67e2fa12b758e62fbe73c7e3781bc93ba
Aditya Sharma <saditya4063@gmail.com>
2026-06-21 18:44:59 +0530

    Time: 1 ms (30.01%), Space: 43.3 MB (5.28%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        int start = 0;
+        int end = s.length()-1;
+        while(start <= end){
+            boolean startChar = (s.charAt(start) >= 97 && s.charAt(start) <= 122) || (s.charAt(start) >= 65 && s.charAt(start) <= 90);
+            boolean endChar = (s.charAt(end) >= 97 && s.charAt(end) <= 122) || (s.charAt(end) >= 65 && s.charAt(end) <= 90);
+            if(!startChar){
+                start++;
+            }else if(!endChar){
+                end--;
+            }else{
+                char temp = sb.charAt(start);
+                sb.setCharAt(start , sb.charAt(end));
+                sb.setCharAt(end , temp);
+                start++;
+                end--;
+            }
+        }
+        return sb.toString();
+    }
+}
